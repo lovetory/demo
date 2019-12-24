64d8835b201f4c4758c5f38c8d191896732f7a0c
TripleZ <me@triplez.cn>
2019-12-24 22:52:02 +0800

    Init commit by @Triple-Z :tada:

---
+++ README.md
@@ -0,0 +1,16 @@
+# Lovetory DEMO
+
+This repo **may be** a quick validation program for LOVETORY developers.
+
+The following github users **may be** the contributors:
+
+[@Triple-Z](github.com/Triple-Z)
+[@arcosx](github.com/arcosx)
+
+## What is lovetory
+
+We still don't know yet.
+
+## Copyright
+
+Copyright &copy; 2019-2020 [lovetory](github.com/lovetory)
---
+++ src/main/java/club/a2os/lovetorydemo/LovetoryDemoApplication.java
@@ -0,0 +1,15 @@
+package club.a2os.lovetorydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovetoryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovetoryDemoApplication.class, args);
+
+        System.out.println("Hello, Lovetory!");
+    }
+
+}
